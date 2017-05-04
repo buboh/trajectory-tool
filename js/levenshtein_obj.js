023eb8667e04fb83a360a141a9579297ba1664c5
buboh <buboh@gmx.at>
2017-05-04 14:43:08 +0200

    levenshtein / edit distance edit

--- js/levenshtein_obj.js
+++ js/levenshtein_obj.js
@@ -1,136 +1,103 @@
+/**
+ * Based on the algorithm at http://en.wikipedia.org/wiki/Levenshtein_distance.
+ */
+
 (function() {
-  'use strict';
-  
-  var collator;
-  try {
-    collator = (typeof Intl !== "undefined" && typeof Intl.Collator !== "undefined") ? Intl.Collator("generic", { sensitivity: "base" }) : null;
-  } catch (err){
-    console.log("Collator could not be initialized and wouldn't be used");
-  }
-  // arrays to re-use
-  var prevRow = [],
-    str2Char = [];
-  
-  /**
-   * Based on the algorithm at http://en.wikipedia.org/wiki/Levenshtein_distance.
-   */
-  var Levenshtein = {
-    /**
-     * Calculate levenshtein distance of the two strings.
-     *
-     * @param str1 String the first string.
-     * @param str2 String the second string.
-     * @param [options] Additional options.
-     * @param [options.useCollator] Use `Intl.Collator` for locale-sensitive string comparison.
-     * @return Integer the levenshtein distance (0 and above).
-     */
-    get: function(str1, str2, options) {
-      var useCollator = (options && collator && options.useCollator);
-      
-      var str1Len = str1.length,
-        str2Len = str2.length;
-      
-      // base cases
-      if (str1Len === 0) return str2Len;
-      if (str2Len === 0) return str1Len;
+    'use strict';
 
-      // two rows
-      var curCol, nextCol, i, j, tmp;
-
-      // initialise previous row
-      for (i=0; i<str2Len; ++i) {
-        prevRow[i] = i;
-        str2Char[i] = str2.charCodeAt(i);
-      }
-      prevRow[str2Len] = str2Len;
-
-      var strCmp;
-      if (useCollator) {
-        // calculate current row distance from previous row using collator
-        for (i = 0; i < str1Len; ++i) {
-          nextCol = i + 1;
-
-          for (j = 0; j < str2Len; ++j) {
-            curCol = nextCol;
-
-            // substution
-            strCmp = 0 === collator.compare(str1.charAt(i), String.fromCharCode(str2Char[j]));
-
-            nextCol = prevRow[j] + (strCmp ? 0 : 1);
-
-            // insertion
-            tmp = curCol + 1;
-            if (nextCol > tmp) {
-              nextCol = tmp;
-            }
-            // deletion
-            tmp = prevRow[j + 1] + 1;
-            if (nextCol > tmp) {
-              nextCol = tmp;
-            }
-
-            // copy current col value into previous (in preparation for next iteration)
-            prevRow[j] = curCol;
-          }
-
-          // copy last col value into previous (in preparation for next iteration)
-          prevRow[j] = nextCol;
-        }
-      }
-      else {
-        // calculate current row distance from previous row without collator
-        for (i = 0; i < str1Len; ++i) {
-          nextCol = i + 1;
-
-          for (j = 0; j < str2Len; ++j) {
-            curCol = nextCol;
-
-            // substution
-            strCmp = str1.charCodeAt(i) === str2Char[j];
-
-            nextCol = prevRow[j] + (strCmp ? 0 : 1);
-
-            // insertion
-            tmp = curCol + 1;
-            if (nextCol > tmp) {
-              nextCol = tmp;
-            }
-            // deletion
-            tmp = prevRow[j + 1] + 1;
-            if (nextCol > tmp) {
-              nextCol = tmp;
-            }
-
-            // copy current col value into previous (in preparation for next iteration)
-            prevRow[j] = curCol;
-          }
-
-          // copy last col value into previous (in preparation for next iteration)
-          prevRow[j] = nextCol;
-        }
-      }
-      return nextCol;
+    /*function idAt(index) {
+        return this[index].ID;
     }
 
-  };
+    Object.defineProperty(Array.prototype, 'idAt', {
+        enumerable: false,
+        value: idAt
+    });*/
 
-  // amd
-  if (typeof define !== "undefined" && define !== null && define.amd) {
+
+    // arrays to re-use
+    const prevRow = [], str2Char = [];
+
+    const Levenshtein = {
+      /**
+       * Calculate levenshtein distance of the two strings.
+       *
+       * @param seq1 String the first string.
+       * @param seq2 String the second string.
+       * @return Integer the levenshtein distance (0 and above).
+       */
+        get: function (seq1, seq2) {
+
+            const seq1Len = seq1.length, seq2Len = seq2.length;
+
+            // base cases
+            if (seq1Len === 0) return seq2Len;
+            if (seq2Len === 0) return seq1Len;
+
+            // two rows
+            let curCol, nextCol, i, j, tmp;
+
+            // initialise previous row
+            for (i = 0; i < seq2Len; ++i) {
+                prevRow[i] = i;
+                str2Char[i] = seq2[i];
+            }
+            prevRow[seq2Len] = seq2Len;
+
+            let strCmp;
+            // calculate current row distance from previous row
+            for (i = 0; i < seq1Len; ++i) {
+                nextCol = i + 1;
+
+                for (j = 0; j < seq2Len; ++j) {
+                    curCol = nextCol;
+
+                    // substution
+                    strCmp = seq1[i] === str2Char[j];
+
+                    nextCol = prevRow[j] + (strCmp ? 0 : 1);
+
+                    // insertion
+                    tmp = curCol + 1;
+                    if (nextCol > tmp) {
+                        nextCol = tmp;
+                    }
+                    // deletion
+                    tmp = prevRow[j + 1] + 1;
+                    if (nextCol > tmp) {
+                        nextCol = tmp;
+                    }
+
+                    // copy current col value into previous (in preparation for next iteration)
+                    prevRow[j] = curCol;
+                }
+
+              // copy last col value into previous (in preparation for next iteration)
+              prevRow[j] = nextCol;
+            }
+
+            return nextCol;
+        }
+
+    };
+
+    // amd
+    if (typeof define !== "undefined" && define !== null && define.amd) {
     define(function() {
       return Levenshtein;
     });
-  }
-  // commonjs
-  else if (typeof module !== "undefined" && module !== null && typeof exports !== "undefined" && module.exports === exports) {
+    }
+    // commonjs
+    else if (typeof module !== "undefined" && module !== null && typeof exports !== "undefined" && module.exports === exports) {
     module.exports = Levenshtein;
-  }
-  // web worker
-  else if (typeof self !== "undefined" && typeof self.postMessage === 'function' && typeof self.importScripts === 'function') {
+    }
+    // web worker
+    else if (typeof self !== "undefined" && typeof self.postMessage === 'function' && typeof self.importScripts === 'function') {
     self.Levenshtein = Levenshtein;
-  }
-  // browser main thread
-  else if (typeof window !== "undefined" && window !== null) {
+    }
+    // browser main thread
+    else if (typeof window !== "undefined" && window !== null) {
     window.Levenshtein = Levenshtein;
-  }
+    }
 }());
 
